a1e046e9ce784d79203b96fa585408c65b82feca
xiaolangshou <xiaolangshou@foxmail.com>
2020-05-14 21:00:11 +0800

    add avcapture demo

---
+++ AVCaptureSessionDemo/AVCaptureSessionDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AVCaptureSessionDemo
+//
+//  Created by Liu Tao on 2020/5/6.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AVCaptureSessionDemo/AVCaptureSessionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVCaptureSessionDemo
+//
+//  Created by Liu Tao on 2020/5/6.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
