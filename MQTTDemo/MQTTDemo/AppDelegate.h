ef0e6b784977e2492c9586b190be3cc458787c06
xiaolangshou <xiaolangshou@foxmail.com>
2020-07-01 07:15:31 +0800

    add new code for mqtt

---
+++ MQTTDemo/MQTTDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MQTTDemo
+//
+//  Created by Liu Tao on 2020/6/30.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MQTTDemo/MQTTDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MQTTDemo
+//
+//  Created by Liu Tao on 2020/6/30.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
