f838ee3e9a4951ef590e0bf2a35544742acaa45b
xiaolangshou <xiaolangshou@foxmail.com>
2020-08-26 17:13:36 +0800

    add anyRtc demo

---
+++ AnyRTC-RTMPC-iOS-master/RTMPCDemo/Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTMPCDemo
+//
+//  Created by 余生丶 on 2019/4/10.
+//  Copyright © 2019 anyRTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
