ee27eb387e8b458f699e7f0513c5b315788de62c
xiaolangshou <xiaolangshou@foxmail.com>
2020-09-03 18:00:18 +0800

    add shallowcopy demo

---
+++ shaollowCopyDemo/shaollowCopyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shaollowCopyDemo
+//
+//  Created by Liu Tao on 2020/9/3.
+//  Copyright © 2020 Liu Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
