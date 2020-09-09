dc78f4d66398320019ef5f0f8ae5a5f153b04370
xiaolangshou <xiaolangshou@foxmail.com>
2020-09-09 15:18:14 +0800

    add shallowCopyDemo

--- shaollowCopyDemo/shaollowCopyDemo/ViewController.m
+++ shaollowCopyDemo/shaollowCopyDemo/ViewController.m
@@ -14,10 +14,63 @@
 
 @implementation ViewController
 
+
+// 浅拷贝和深拷贝本质是地址是否相同
+// 1.非容器不可变对象 NSString
+// 2.非容器可变对象 NSMutableString
+// 3.容器类不可变对象 NSArray
+// 4.容器类可变对象 NSMutableArray
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+//    [self func1];
+//    [self func2];
+    [self func3];
 }
+
+// 非容器不可变对象
+- (void)func1 {
+    
+    NSString *str1 = @"非容器不可变对象";
+    NSString *str2 = [str1 copy];
+    NSString *str3 = [str1 mutableCopy];
+    
+    NSLog(@"str1:%p, class: %@", str1, [str1 class]);
+    NSLog(@"str2:%p, class: %@", str2, [str2 class]);
+    NSLog(@"str3:%p, cladd: %@", str3, [str3 class]);
+}
+
+// 非容器可变对象
+- (void)func2 {
+    
+    NSString *str1 = [NSMutableString stringWithFormat:@"非容器可变对象"];
+    NSString *str2 = [str1 copy];
+    NSString *str3 = [str1 mutableCopy];
+    
+    NSLog(@"str1:%p, class: %@", str1, [str1 class]);
+    NSLog(@"str2:%p, class: %@", str2, [str2 class]);
+    NSLog(@"str3:%p, class: %@", str3, [str3 class]);
+}
+ 
+// 容器不可变对象
+- (void)func3 {
+    
+    NSString *str = [NSString stringWithFormat:@"容器不可变对象"];
+    NSArray *array = [NSArray arrayWithObjects:str, nil];
+    NSArray *copyArray = [array copy];
+    NSArray *mutableArray = [array mutableCopy];
+    
+    NSLog(@"array:%p, class:%@", array, [array class]);
+    NSLog(@"copyArray:%p, class:%@", copyArray, [copyArray class]);
+    NSLog(@"mutableArray:%p, class:%@", mutableArray, [mutableArray class]);
+}
+
+// 容器可变对象
+- (void)func4 {
+    
+}
+
 
 
 @end
