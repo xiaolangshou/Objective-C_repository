a63ab9d5cb723c903ad446ac915b6b4cd6cbeb5c
xiaolangshou <xiaolangshou@foxmail.com>
2020-09-15 19:27:27 +0800

    add keyatchived demo

--- shaollowCopyDemo/shaollowCopyDemo/ViewController.m
+++ shaollowCopyDemo/shaollowCopyDemo/ViewController.m
@@ -14,75 +14,18 @@
 
 @implementation ViewController
 
-<<<<<<< HEAD
-
 // 浅拷贝和深拷贝本质是地址是否相同
-// 1.非容器不可变对象 NSString
-// 2.非容器可变对象 NSMutableString
-// 3.容器类不可变对象 NSArray
-// 4.容器类可变对象 NSMutableArray
-=======
+
 /*
  1.非容器不可变对象 NSString
  2.非容器可变对象 NSMutableString
  3.容器不可变对象 NSArray
  4.容器可变对象 NSMutableArray
  */
->>>>>>> 303e81cc25c4f8f1dbd38412e19a4886ef55bee8
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-<<<<<<< HEAD
-//    [self func1];
-//    [self func2];
-    [self func3];
-}
 
-// 非容器不可变对象
-- (void)func1 {
-    
-    NSString *str1 = @"非容器不可变对象";
-    NSString *str2 = [str1 copy];
-    NSString *str3 = [str1 mutableCopy];
-    
-    NSLog(@"str1:%p, class: %@", str1, [str1 class]);
-    NSLog(@"str2:%p, class: %@", str2, [str2 class]);
-    NSLog(@"str3:%p, cladd: %@", str3, [str3 class]);
-}
-
-// 非容器可变对象
-- (void)func2 {
-    
-    NSString *str1 = [NSMutableString stringWithFormat:@"非容器可变对象"];
-    NSString *str2 = [str1 copy];
-    NSString *str3 = [str1 mutableCopy];
-    
-    NSLog(@"str1:%p, class: %@", str1, [str1 class]);
-    NSLog(@"str2:%p, class: %@", str2, [str2 class]);
-    NSLog(@"str3:%p, class: %@", str3, [str3 class]);
-}
- 
-// 容器不可变对象
-- (void)func3 {
-    
-    NSString *str = [NSString stringWithFormat:@"容器不可变对象"];
-    NSArray *array = [NSArray arrayWithObjects:str, nil];
-    NSArray *copyArray = [array copy];
-    NSArray *mutableArray = [array mutableCopy];
-    
-    NSLog(@"array:%p, class:%@", array, [array class]);
-    NSLog(@"copyArray:%p, class:%@", copyArray, [copyArray class]);
-    NSLog(@"mutableArray:%p, class:%@", mutableArray, [mutableArray class]);
-}
-
-// 容器可变对象
-- (void)func4 {
-    
-}
-
-
-=======
     [self func1];
     // [self func2];
     // [self func3];
@@ -163,6 +106,5 @@
     NSLog(@"mutableCopy对象");
     NSLog(@"array3[0]: %p, array3[1]: %p", array3[0], array3[1]);
 }
->>>>>>> 303e81cc25c4f8f1dbd38412e19a4886ef55bee8
 
 @end
